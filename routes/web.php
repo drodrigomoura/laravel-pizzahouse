b1b6d203587aeab3e41cce566ae03ab47b56db18
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-07-21 19:12:16 -0300

    publicar

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/pizzas/{id}', 'PizzaController@show')->name('pizzas.show')->middlew
 Route::delete('/pizzas/{id}', 'PizzaController@destroy')->name('pizzas.destroy')->middleware('auth');   
 
 Auth::routes([
-    'register' => false 
+    'register' => true
 ]);
 
 Route::get('/home', 'HomeController@index')->name('home');
